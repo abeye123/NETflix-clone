c4dff6aa75e3f7972391bc4fcd150ea89931fc2e
abiyu <rofkeru2@gmail.com>
2020-12-19 22:29:55 -0500

    update

---
+++ src/requests.js
@@ -0,0 +1,16 @@
+const API_KEY="b1c5c3d82bad6fa7c8b4b07c8883a2e8";
+
+const requests = {
+
+    fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+    fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+    fetchTopRatedMovies: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+    fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+    fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+    fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+    fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+    fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
+
+};
+
+export default requests;
